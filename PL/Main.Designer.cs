94437b00ca2921bbd8932bc226342b3342f59639
Brandon Daniel <brandanimb@gmail.com>
2019-10-03 14:59:24 -0600

    Agregar archivos de proyecto.

---
+++ PL/Main.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace PL
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Encriptar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.SystemColors.MenuText;
+            this.Title.Font = new System.Drawing.Font("Fira Mono", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(223, 28);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(354, 42);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Project Ariandel";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Exit
+            // 
+            this.Exit.AutoSize = true;
+            this.Exit.BackColor = System.Drawing.SystemColors.ControlText;
+            this.Exit.Font = new System.Drawing.Font("Fira Mono", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.ForeColor = System.Drawing.Color.White;
+            this.Exit.Location = new System.Drawing.Point(361, 544);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(79, 32);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "EXIT";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Fira Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(42, 138);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(716, 232);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Encriptar
+            // 
+            this.Encriptar.AutoSize = true;
+            this.Encriptar.BackColor = System.Drawing.SystemColors.ControlText;
+            this.Encriptar.Font = new System.Drawing.Font("Fira Mono", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Encriptar.ForeColor = System.Drawing.SystemColors.Control;
+            this.Encriptar.Location = new System.Drawing.Point(306, 438);
+            this.Encriptar.Name = "Encriptar";
+            this.Encriptar.Size = new System.Drawing.Size(188, 38);
+            this.Encriptar.TabIndex = 3;
+            this.Encriptar.Text = "Encriptar";
+            this.Encriptar.Click += new System.EventHandler(this.Encriptar_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PL.Properties.Resources._516683;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.Encriptar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ariandel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label Exit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Encriptar;
+    }
+}
+
---
+++ PL/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PL
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Encriptar_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            
+
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+            MessageBox.Show("Nice try, try again.");
+        }
+    }
+}
